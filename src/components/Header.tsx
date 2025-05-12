7061c76afb75c2641819131874a047f16adc918e
Ryan Blaquiere <ryan.blaquiere@gmail.com>
2025-05-11 20:38:27 -0400

    fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
