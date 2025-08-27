0a5a1749a0e09f55fe6344a9a4d2d9a26e749464
Ryan Blaquiere <ryan.blaquiere@live.com>
2025-08-27 19:49:07 -0400

    upd

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -34,7 +34,9 @@ const skills = [
 const About = () => (
   <section className="bg-slate-200 dark:bg-slate-900 py-20 px-6 max-w-4xl mx-auto text-center">
     <h2 className="text-slate-700 dark:text-slate-200 text-3xl font-bold mb-6">About Me</h2>
-    <p className="text-slate-700 dark:text-slate-200 text-lg mb-12">Hi! I'm a Lead Software Engineer with 10+ years of full-stack development experience across enterprise SaaS platforms and agile teams. I have a proven track record in modernizing legacy systems, leading front-end and back-end development. I am adept at mentoring teams, optimizing system performance, and delivering secure, scalable solutions.</p>
+    <p className="text-slate-700 dark:text-slate-200 text-lg mb-12">Hi! I'm a Lead Software Engineer with 10+ years of full-stack development experience across enterprise SaaS platforms and agile teams. 
+                                                                    I have a proven track record in modernizing legacy systems, leading front-end and back-end development. 
+                                                                    I am adept at mentoring teams, optimizing system performance, and delivering secure, scalable solutions.</p>
     
     <h2 className="text-slate-700 dark:text-slate-200 text-3xl font-bold mb-6">Skills</h2>
     <div className="flex flex-wrap justify-center gap-3">
